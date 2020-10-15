8234dcb8b79273203e85169a29729eb5943fe21a
yangxg <zmrenwu@163.com>
2020-10-15 13:18:44 +0900

    upgrade to docker/build-push-action@v2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+mkdir -p /etc/nginx/conf.d
+
 # https://crontab.guru/every-week
 echo "0 0 * * 0 root certbot renew >> /var/log/cron.log 2>&1" >/etc/cron.d/certbot
 touch /var/log/cron.log
